c4043a54a4e24d25146b628d955efbf20d4daf0d
SergeyBirukov <birukovsergej121@gmail.com>
2023-04-29 20:59:03 +0500

    Reworked weapon

---
+++ Assets/Scripts/Weapons/AOEWeaponHitZone.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Weapons
+{
+    public class AOEWeaponHitZone: MonoBehaviour
+    {
+        [SerializeField] private Transform topLeft;
+        [SerializeField] private Transform bottomRight;
+        public float topLeftX;
+        public float topLeftY;
+        public float bottomRightX;
+        public float bottomRightY;
+
+        private void Start()
+        {
+            topLeftX = topLeft.transform.position.x;
+            topLeftY = topLeft.transform.position.y;
+            bottomRightX = bottomRight.transform.position.x;
+            bottomRightY = bottomRight.transform.position.y;
+        }
+    }
+}

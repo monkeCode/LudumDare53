f82b57a6e112173a0e01c93253d578c44e50e40a
Georgy <georgy.tertychniy@yandex.ru>
2023-04-29 13:20:12 +0300

    textures

---
+++ Assets/Scripts/Entities/Entity.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    [SerializeField] protected int hp;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

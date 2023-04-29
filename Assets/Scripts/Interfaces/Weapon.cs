ceb0a4c71cdeb27411dbb6d640dde0ec5caaf808
SergeyBirukov <birukovsergej121@gmail.com>
2023-04-29 16:39:47 +0500

    Added weapon abstract class

---
+++ Assets/Scripts/Interfaces/Weapon.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Interfaces
+{
+    public abstract class Weapon: MonoBehaviour
+    {
+        public abstract float Damage { get; }
+        public abstract float Cooldown { get; }
+        public abstract void Attack();
+    }
+}

4ee6275976a90ac4e98f20c19cda55551ab34212
lessy2014 <lessik03@gmail.com>
2023-04-29 17:06:05 +0500

    Delivery points, destination marker and abstract debuffs

---
+++ Assets/Delivery.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Delivery
+    {
+        public float Reward;
+        public DeliveryPoint Destination;
+        public Debuff Debuff;
+
+        public Delivery(DeliveryPoint departure, DeliveryPoint destination, Debuff debuff)
+        {
+            Reward = CalculateReward(departure, destination, debuff);
+            Destination = destination;
+            Debuff = debuff;
+        }
+
+        private float CalculateReward(DeliveryPoint departure, DeliveryPoint destination, Debuff debuff)
+        {
+            var distance = Vector2.Distance(departure.transform.position, destination.transform.position);
+            var reward = distance * debuff.RewardRatio;
+            return reward;
+        }
+    }
+}

96293862cd18907bfcc05f475029943db998636c
Georgy <georgy.tertychniy@yandex.ru>
2023-04-30 10:51:14 +0300

    timer

--- Assets/Scripts/WaveGenerator.cs
+++ Assets/Scripts/WaveGenerator.cs
@@ -9,13 +9,17 @@ public class WaveGenerator : MonoBehaviour
 {
     [SerializeField] private Wave[] _waves;
     [SerializeField] private Wave[] _specialWaves;
+    [SerializeField]  private Wave[] _bossWave;
     [SerializeField] private float _radius;
     [FormerlySerializedAs("_wayDelay")] [SerializeField] private float _delay;
     private float _lastTime;
 
+    public static float Timer { get; private set; }
+    
     private void Update()
     {
-        if (Time.timeSinceLevelLoad - _lastTime >= _delay)
+        Timer += Time.deltaTime;
+        if (Timer - _lastTime >= _delay)
         {
             if (Random.Range(0, 100) <= 1)
             {
@@ -28,7 +32,7 @@ public class WaveGenerator : MonoBehaviour
                 _waves[i].Spawn(Player.Player.Instance.transform.position, _radius);
             }
 
-            _lastTime = Time.timeSinceLevelLoad;
+            _lastTime = Timer;
         }
     }
 

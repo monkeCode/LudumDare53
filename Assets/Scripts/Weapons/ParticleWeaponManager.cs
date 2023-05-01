ea76c6fde4c791b63ae0009a3f97fa070c8f2844
Georgy <georgy.tertychniy@yandex.ru>
2023-05-01 17:06:28 +0300

    battle toad

---
+++ Assets/Scripts/Weapons/ParticleWeaponManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Weapons
+{
+    public class ParticleWeaponManager:Weapon
+    {
+        [SerializeField] private Weapon _prefab;
+        private List<Weapon> _activeWeapons = new();
+
+        public  override void Attack()
+        {
+            
+        }
+
+        protected override void Update()
+        {
+            if (_activeWeapons.Count < Player.Player.Instance.AtkCount)
+            {
+                _activeWeapons.Add(Instantiate(_prefab, Player.Player.Instance.transform.position, Quaternion.identity));
+                for(int i = 1; i < _activeWeapons[0].Lvl; i++)
+                    _activeWeapons[^1].LvlUp();
+                return;
+            }
+            if (_activeWeapons.Count <= Player.Player.Instance.AtkCount) return;
+            var weapon = _activeWeapons[0];
+            _activeWeapons.Remove(weapon);
+            Destroy(weapon);
+        }
+
+        public override void LvlUp()
+        {
+            foreach (var weapon in _activeWeapons)
+            {
+                weapon.LvlUp();
+            }
+        }
+    }
+}

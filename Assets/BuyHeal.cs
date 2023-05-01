ed52b93156a2497c81d2f54f32fdbef5c49ae856
lessy2014 <lessik03@gmail.com>
2023-05-01 22:39:57 +0500

    Can buy heal

---
+++ Assets/BuyHeal.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyHeal : MonoBehaviour
+{
+    [SerializeField] private int price;
+    [SerializeField] private int heal;
+
+    public void BuyHealing()
+    {
+        if (Player.Player.Instance.Money <= price) return;
+        Player.Player.Instance.Money -= price;
+        Player.Player.Instance.Heal(heal);
+    }
+}
